9e1d9f40f3436df37e1fb1d6af46695451873a0d
xTheoxo <theoar30210@gmail.com>
2026-07-29 00:10:29 +0200

    Fix

--- centerpad/MainWindow.xaml.cs
+++ centerpad/MainWindow.xaml.cs
@@ -5,10 +5,16 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;   // <-- ajoute celle-ci
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using System.CodeDom.Compiler;
+using System.Reflection;
+using Microsoft.CSharp;
+using System.Diagnostics;
 
 namespace centerpad
 {
@@ -17,18 +23,35 @@ namespace centerpad
     /// </summary>
     public partial class MainWindow : Window
     {
-        string Path = "Extensions";
-        string version = "0.1";
 
-        
+        string? appchoisi;
+
+        string Path_extension = "Extensions";
+        string version = "0.1.2.1";
+
+
         public MainWindow()
         {
             InitializeComponent();
 
             label.Content = version;
 
-            if (!Directory.Exists(Path)) 
-                Directory.CreateDirectory(Path);
+            if (!Directory.Exists(Path_extension))
+                Directory.CreateDirectory(Path_extension);
+
+
+            string[] files = Directory.GetFiles(Path_extension, "*.exe");
+            select_app.ItemsSource = files;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            appchoisi = select_app.SelectedItem.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(appchoisi);
         }
     }
 }
